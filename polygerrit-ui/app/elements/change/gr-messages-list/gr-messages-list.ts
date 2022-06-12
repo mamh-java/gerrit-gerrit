74b708e756ffaf2a63cecb19fb116c6328d745c1
bright.ma <bright.ma@blackshark.com>
2022-06-12 21:00:45 +0800

    polygerrit-ui: gr-messages-list.ts: sort messages list by patch set number
    Signed-off-by: bright.ma <bright.ma@blackshark.com>
    Change-Id: I30ef884283a611dcdf9790327136f9b547174402
    Signed-off-by: bright.ma <bright.ma@blackshark.com>

--- polygerrit-ui/app/elements/change/gr-messages-list/gr-messages-list.ts
+++ polygerrit-ui/app/elements/change/gr-messages-list/gr-messages-list.ts
@@ -437,6 +437,19 @@ export class GrMessagesList extends KeyboardShortcutMixin(PolymerElement) {
   _combinedMessagesChanged(combinedMessages?: CombinedMessage[]) {
     if (combinedMessages) {
       if (combinedMessages.length === 0) return;
+
+      combinedMessages.sort((o1, o2) =>{ // 排序，按照patch set number 来排序。
+        try {
+          if (o1 && o2) {
+            return <number>o1._revision_number - <number>o2._revision_number;
+          } else {
+            return 0;
+          }
+        } catch (e) {
+          return 0;
+        }
+      });
+
       const tags = combinedMessages.map(
         message =>
           message.tag || (message as FormattedReviewerUpdateInfo).type || 'none'
